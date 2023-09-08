af182e9384db6c99f61dcd6f2418fd6ddf35ca19
felipe-luis <105252192+felipe-luis@users.noreply.github.com>
2023-09-08 12:41:41 -0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,47 @@
+import { Modal } from "./modal.js"
+import { AlertError} from "./alert-error.js"
+
+//variáveis 
+
+const form = document.querySelector('form')
+const inputWeight = document.querySelector('#weight')
+const inputHeight = document.querySelector('#height')
+
+
+
+
+
+form.onsubmit = function(event) {
+    event.preventDefault()
+    
+    const weight = Number(inputWeight.value)
+    const height = Number(inputHeight.value)
+
+    const showAlertError = notANumber(weight) || notANumber(height)
+
+    if(showAlertError){
+        AlertError.open()
+        return;
+    }
+    
+    AlertError.close()
+    
+    const result = IMC(weight, height) 
+    const message = `Seu IMC é de ${result}`
+
+    Modal.message.innerText = message
+    Modal.open()
+    
+}
+
+
+function notANumber(value) {
+    return isNaN(value) || value == ""
+}
+
+
+
+
+function IMC(weight,height) {
+    return (weight/(height/100)**2).toFixed(2)
+}
